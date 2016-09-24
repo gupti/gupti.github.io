1afa8921298a15df10e1e51dd4eab98246d66935
gupti <paulgupti@gmail.com>
2016-09-24 14:11:29 -0400

    Added files

--- main.js
+++ main.js
@@ -2,32 +2,17 @@
 var selected = 'home';
 
 // Change the currently displayed page
-load = function (pageName) {
-  if (pageName !== selected){
-
+load = function (pageName)
+{
+  if (pageName !== selected)
+  {
     // Change displayed content block
     document.getElementById(selected).style.display = 'none';
-    document.getElementById(pageName).style.display = 'block';
+    document.getElementById(pageName).style.display = '';
 
-    // Change title text
-    document.getElementById(selected + '-title').className += ' hidden';
-    setTimeout(function(){
-      document.getElementById(selected + '-title').style.display = 'none';
-      document.getElementById(pageName + '-title').style.display = 'inline-block';
-      setTimeout(function(){document.getElementById(pageName + '-title').className = document.getElementById(selected + '-title').className.replace(/(?:^|\s)hidden(?!\S)/g, '');}, 50);
-      selected = pageName;
-    }, 200);
+    document.getElementById(selected + '-btn').style.textDecoration = '';
+    document.getElementById(pageName + '-btn').style.textDecoration = 'underline';
 
-    // Change which button is pushed in
-    document.getElementById(selected + '-btn').className = document.getElementById(selected + '-btn').className.replace(/(?:^|\s)btn-left(?!\S)/g, '');
-    document.getElementById(pageName + '-btn').className += ' btn-left';
+    selected = pageName;
   }
-}
-
-
-// Setup on page load
-function setup() {
-  //document.getElementById('proj').style.display = 'none';
-  //document.getElementById('serv').style.display = 'none';
-  return;
 }
