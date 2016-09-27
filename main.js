77be0dce2a59b36dcead8a2ad5ebfa9e0016b6f2
gupti <paulgupti@gmail.com>
2016-09-27 08:56:24 -0400

    Hacked together basic animations

--- main.js
+++ main.js
@@ -7,8 +7,18 @@ load = function (pageName)
   if (pageName !== selected)
   {
     // Change displayed content block
-    document.getElementById(selected).style.display = 'none';
-    document.getElementById(pageName).style.display = '';
+    var elementToHide = document.getElementById(selected);
+    var elementToShow =  document.getElementById(pageName);
+    elementToHide.className += ' container-hide';
+
+    setTimeout(function()
+    {
+      elementToHide.className += ' container-none';
+      elementToShow.className = 'container container-hide';
+      setTimeout(function() {
+        elementToShow.className = 'container';
+      }, 100);
+    }, 350);
 
     document.getElementById(selected + '-btn').style.textDecoration = '';
     document.getElementById(pageName + '-btn').style.textDecoration = 'underline';
